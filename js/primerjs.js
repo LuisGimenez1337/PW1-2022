be81c7d390a15d4b5b3f1d8a68ce7aee3652993a
LuisGimenez1337 <101958302+LuisGimenez1337@users.noreply.github.com>
2022-04-02 21:24:13 -0400

    ordenando

--- js/primerjs.js
+++ js/primerjs.js
@@ -2,9 +2,9 @@
 function light(sw) {
   var pic;
   if (sw == 0) {
-    pic = "img/pic_bulboff.gif"
+    pic = "../img/pic_bulboff.gif"
   } else {
-    pic = "img/pic_bulbon.gif"
+    pic = "../img/pic_bulbon.gif"
   }
   document.getElementById('myImage').src = pic;
 }
